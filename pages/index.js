d1b6dcebb6968e9db380aa5877392ed9ddbcd6fb
Gabriel Barros <gabrielbarrosdeo@hotmail.com>
2023-06-20 13:59:46 -0300

    adicionado codigos de mudança par aula 2.4

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,3 @@
-import Link from '../src/components/Link'
-import React from 'react'
+import HomeScreen from '../src/screens/HomeScreen';
 
-function Title({ children, as }) {
-    const Tag = as;
-    return (
-        <React.Fragment>
-            <Tag>
-                {children}
-            </Tag>
-            <style jsx>{`
-            ${Tag}{
-                color:red;                
-            }`}
-
-            </style>
-        </React.Fragment>
-    )
-}
-
-export default function HomePage() {
-    return (
-        <div>            
-            <Title as="h1">Alura Cases - Home</Title>
-            <Link href="/faq">
-                ir para o FAQ
-            </Link>
-        </div>
-    )
-}
+export default HomeScreen;
